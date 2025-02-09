abdc09e32861683b917ba941183f071d2b010aad
ABrain-One <112828851+ABrain-One@users.noreply.github.com>
2025-02-09 10:19:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     author="ABrain One and contributors",
     author_email="AI@ABrain.one",
     url="https://ABrain.one",
-    packages=find_packages(include=["ab.plot.*"]),
+    packages=find_packages(include=["ab.*"]),
     install_requires=read_requirements(),
     classifiers=[
         "Programming Language :: Python :: 3",
