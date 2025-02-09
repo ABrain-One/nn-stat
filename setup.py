7db8fe4d7c49688b6c8ec113c91a18a3847bda2d
ABrain-One <112828851+ABrain-One@users.noreply.github.com>
2025-02-09 12:32:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read_readme():
 
 setup(
     name="nn-stat",
-    version="1.0.5",
+    version="1.0.6",
     description="Neural Network Performance Analysis",
     long_description=read_readme(),
     long_description_content_type="text/markdown",
