960825409631083618fb8f6910bf2295b956382b
ABrain-One <112828851+ABrain-One@users.noreply.github.com>
2025-02-11 20:00:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,10 +14,15 @@ def read_readme():
     if readme_path.exists():
         return readme_path.read_text(encoding="utf-8")
     return ""
+    
+def version():
+    with open('version', 'r') as file:
+      v = file.readline()
+    return v
 
 setup(
     name="nn-stat",
-    version="1.0.7",
+    version=version(),
     description="Neural Network Performance Analysis",
     long_description=read_readme(),
     long_description_content_type="text/markdown",
